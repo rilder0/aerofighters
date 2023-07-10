01bc654c321c0c86895b83ed650a41b4deefe419
Rilder <79425480+rilder0@users.noreply.github.com>
2023-07-10 16:11:22 -0300

    u

---
+++ Aero Fighters/Assets/Scripts/UI/Munição.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Munição : MonoBehaviour
+{
+    public int muniçãotirosimples;
+    public bool estaatirando;
+
+    public Text Munição1;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Munição1.text = muniçãotirosimples.ToString("∞");
+        if (Input.GetKey(KeyCode.Space) && !estaatirando && muniçãotirosimples > 0)
+        {
+            estaatirando = true;
+            muniçãotirosimples--;
+            estaatirando = false;
+        }
+        Debug.Log("Munição = " + muniçãotirosimples);
+    }
+}
